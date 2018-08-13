451c39623ff9a5df1de1e15c93a23bcfb50a4a4b
FCTGKevin <kevin.mckeen@flightcentre.ca>
2018-08-13 13:19:59 -0700

    Margin feature.180813

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World. Welcome to GIT! Let the branching begin.");
+            Console.WriteLine("Hello World. Welcome to GIT! Let the branching begin!");
             Console.ReadKey();
         }
     }
