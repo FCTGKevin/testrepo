71f693d77ab147e68536abf3f8291fc76fcaac08
FCTGKevin <kevin.mckeen@flightcentre.ca>
2018-08-13 13:59:04 -0700

    Removed second line of text.

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -11,7 +11,6 @@ namespace HelloWorldConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World. Welcome to GIT! Let the branching begin!");
-            Console.WriteLine("Learning how to GIT is an invaluable skill!");
             Console.ReadKey();
         }
     }
